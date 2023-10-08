1e7c76d42da181389671114691590ee55888062c
Zacharie Legault <zacharie@hudsonlegault.net>
2023-10-08 21:12:14 +0000

    Config file for training script

--- src/drgnet/scripts/train.py
+++ src/drgnet/scripts/train.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
+import argparse
+from pathlib import Path
+
 import torch
+import yaml
+from pydantic import BaseModel
 from sklearn.metrics import accuracy_score, cohen_kappa_score
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
@@ -14,20 +19,37 @@ from drgnet.transforms import GaussianDistance
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, help="Path to YAML config file.")
+    args = parser.parse_args("--config configs/aptos.yaml".split(" "))
+
+    config_path = Path(args.config)
+    config = TrainConfig.parse_yaml(config_path)
+    print(config)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    radius_sigma_px = 10
     transform = Compose(
         [
-            RadiusGraph(3 * radius_sigma_px),
-            GaussianDistance(sigma=radius_sigma_px),
+            RadiusGraph(3 * config.dataset.distance_sigma_px),
+            GaussianDistance(sigma=config.dataset.distance_sigma_px),
             ToSparseTensor(),
         ]
     )
-    dataset = Aptos(root="data/aptos/", transform=transform)
-    train, val = dataset.split(0.8, 0.2)
-    train_loader = DataLoader(train, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val, batch_size=64)
+
+    if config.dataset.name.lower() == "aptos":
+        dataset = Aptos(
+            root=config.dataset.root,
+            transform=transform,
+            num_keypoints=config.dataset.num_keypoints,
+            sigma=config.dataset.sift_sigma,
+        )
+    else:
+        raise ValueError(f"Unknown dataset: {config.dataset.name}")
+
+    train, val = dataset.split(*config.dataset.split)
+    train_loader = DataLoader(train, batch_size=config.batch_size, shuffle=True)
+    val_loader = DataLoader(val, batch_size=config.batch_size, shuffle=False)
 
     model = DRGNet(dataset.num_features, 32, 4, 50, dataset.num_classes).to(device)
     optim = Adam(model.parameters(), lr=0.01)
@@ -67,6 +89,26 @@ def main():
 
     print(f"Best validation accuracy: {max(acc):.4f}")
     print(f"Best validation kappa: {max(kappa):.4f}")
+
+
+class TrainConfig(BaseModel):
+    dataset: "DatasetConfig"
+    batch_size: int
+
+    @classmethod
+    def parse_yaml(cls, path: Path) -> "TrainConfig":
+        with open(path, "r") as f:
+            config = yaml.safe_load(f)
+        return cls(**config)
+
+
+class DatasetConfig(BaseModel):
+    name: str
+    root: str
+    split: list[float]
+    num_keypoints: int
+    sift_sigma: float
+    distance_sigma_px: float
 
 
 if __name__ == "__main__":
