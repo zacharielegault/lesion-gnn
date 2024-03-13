76109a532a32ba2bafc1aa3cff21923ada9b5b80
Zacharie Legault <32556865+zacharielegault@users.noreply.github.com>
2024-03-13 15:34:04 -0400

    Update train script

--- lesion-aware-transformer/train.py
+++ lesion-aware-transformer/train.py
@@ -3,16 +3,56 @@ from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 
-from fundus_datamodules import AptosClassificationDataModule
+from fundus_datamodules import DDRClassificationDataModule
 from lesion_aware_transformer import LesionAwareTransformer
 from lesion_gnn.callbacks import ConfusionMatrixCallback
 
 
 def main():
-    dm = AptosClassificationDataModule("data/APTOS2019", batch_size=16, img_size=(512, 512), num_workers=6)
+    NUM_WORKERS = 6
+
+    # From the paper
+    IMG_SIZE = (512, 512)
+    BACKBONE = "resnet50"
+    W_TRIPLET = 0.04
+    W_CONSISTENCY = 0.01
+    PRE_LN = False
+    NUM_HEADS = 8
+    FFN_MULTIPLIER = 1
+    NUM_FILTERS = 4
+
+    # Unspecified in the paper
+    MAX_EPOCHS = 20
+    BATCH_SIZE = 16
+    EMBED_DIM = 256
+    TRIPLET_MARGIN = 1.0
+    OPTIMIZER = "adam"
+    LR = 1e-4
+    WEIGHT_DECAY = 1e-4
+    DROPOUT = 0.1
+
+    dm = DDRClassificationDataModule(
+        "data/DDR-dataset", batch_size=BATCH_SIZE, img_size=IMG_SIZE, num_workers=NUM_WORKERS
+    )
     dm.setup("fit")
 
-    model = LesionAwareTransformer(num_classes=5, embed_dim=256, num_filters=4)
+    model = LesionAwareTransformer(
+        num_classes=5,
+        embed_dim=EMBED_DIM,
+        num_filters=NUM_FILTERS,
+        backbone=BACKBONE,
+        pretrained=True,
+        num_heads=NUM_HEADS,
+        dropout=DROPOUT,
+        ffn_multiplier=FFN_MULTIPLIER,
+        pre_ln=PRE_LN,
+        triplet_margin=TRIPLET_MARGIN,
+        w_triplet=W_TRIPLET,
+        w_consistency=W_CONSISTENCY,
+        optimizer=OPTIMIZER,
+        lr=LR,
+        weight_decay=WEIGHT_DECAY,
+    )
 
     logger = WandbLogger(
         project="lesion-aware-transformer",
@@ -32,7 +72,7 @@ def main():
         ConfusionMatrixCallback(),
     ]
 
-    trainer = Trainer(devices=[0], logger=logger, max_epochs=10, log_every_n_steps=1, callbacks=callbacks)
+    trainer = Trainer(devices=[0], logger=logger, max_epochs=MAX_EPOCHS, log_every_n_steps=1, callbacks=callbacks)
     trainer.fit(model, datamodule=dm)
 
     dm.setup("test")
